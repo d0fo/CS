b412049fdc259fd7920b0f539145c7f7774e2338
d0fo <z_parker@mail.ru>
2022-08-06 14:21:42 +0300

    Task6 done

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter integer number");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n%2==0)
+{
+    Console.WriteLine("This number is even");
+}
+else
+{
+    Console.WriteLine("This number is uneven");
+}
