0527f56875064ef6477d8a1689a3493029d24f10
d0fo <z_parker@mail.ru>
2022-08-06 13:12:49 +0300

    Task2 finished

---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter first number");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if(n1>n2)
+{
+    Console.WriteLine("First number is bigger then second number");
+}
+if(n2>n1)
+{
+        Console.WriteLine("Second number is bigger then first number");
+}
+if(n1==n2) 
+{
+        Console.WriteLine("First number is equal tosecond number");
+}
