91ab7ecb7009495def375f58f3da8e2ce71f9e38
d0fo <z_parker@mail.ru>
2022-08-06 13:52:00 +0300

    Task4 added

---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter first number");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+if(n2>max)
+{
+max=n2;
+}
+if(n3>max)
+{
+max=n3;
+}
+Console.Write("Biggest number from this three is: ");
+Console.Write(max);
