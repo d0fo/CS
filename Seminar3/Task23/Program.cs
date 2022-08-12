0855e84cdf5e2594742120b552d46e9c270874f1
d0fo <z_parker@mail.ru>
2022-08-12 16:00:41 +0300

    Task23 is done.

---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.WriteLine("Cubes from 1 to this number are:");
+while(count<=n)
+{
+    Console.WriteLine(Math.Pow(count,3));
+    count++;
+}
