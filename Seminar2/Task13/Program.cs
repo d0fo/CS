837c1ae2d2151ed5e1b3e86b2d25d0b6a5342523
d0fo <z_parker@mail.ru>
2022-08-12 14:39:32 +0300

    Task13 mistakes fixed

--- Seminar2/Task13/Program.cs
+++ Seminar2/Task13/Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Enter integer number");
 long n = Convert.ToInt64(Console.ReadLine());
 char[] array = n.ToString().ToCharArray();
-Console.WriteLine($"Third left digit in this number is: {array[2]}");
+if (n>99) Console.WriteLine($"Third left digit in this number is: {array[2]}");
+else if (n<-99) Console.WriteLine($"Third left digit in this number is: {array[3]}");
+else Console.WriteLine("There is no third left digit in this number");
+
