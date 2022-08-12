fd72fe5831f9e9d8f2adeb8b54b8bd50409f995e
d0fo <z_parker@mail.ru>
2022-08-12 08:50:17 +0300

    Task10 mistakes fixed.

--- Seminar2/Task10/Program.cs
+++ Seminar2/Task10/Program.cs
@@ -2,5 +2,8 @@
 Console.WriteLine("Enter tree-digit number");
 int n=Convert.ToInt32(Console.ReadLine());
 if (n<=100|n>999) Console.WriteLine("This is no tree-digit number");
+else
+{
 int a=n/10%10;
 Console.WriteLine("Second digit is: " +a);
+}
