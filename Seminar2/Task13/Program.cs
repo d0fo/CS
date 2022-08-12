be38cc0268262c4db06b5bfcdb62ea83c1407ea6
d0fo <z_parker@mail.ru>
2022-08-12 08:46:14 +0300

    Task13 is done.

--- Seminar2/Task13/Program.cs
+++ Seminar2/Task13/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("QQ");
-Console.WriteLine("qw");
-
+Console.WriteLine("Enter integer number");
+long i = Convert.ToInt64(Console.ReadLine());
+char[] array = i.ToString().ToCharArray();
+Console.WriteLine($"Third left digit in this number is: {array[2]}");
