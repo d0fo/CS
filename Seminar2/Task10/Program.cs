a9127c509272d5fa3625269c4024167b4075541a
d0fo <z_parker@mail.ru>
2022-08-12 07:36:46 +0300

    Task10 is done.

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter tree-digit number");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n<=100|n>999) Console.WriteLine("This is no tree-digit number");
+int a=n/10%10;
+Console.WriteLine("Second digit is: " +a);
