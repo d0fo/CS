29c3140b3330d85d45c19994b7fb0610434efd96
d0fo <z_parker@mail.ru>
2022-08-12 09:09:54 +0300

    Task15 is done.

--- Seminar2/Task13/Program.cs
+++ Seminar2/Task13/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Enter integer number");
-long i = Convert.ToInt64(Console.ReadLine());
-char[] array = i.ToString().ToCharArray();
+long n = Convert.ToInt64(Console.ReadLine());
+char[] array = n.ToString().ToCharArray();
 Console.WriteLine($"Third left digit in this number is: {array[2]}");
--- Seminar2/Task15/Program.cs
+++ Seminar2/Task15/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("f");
+Console.WriteLine("Enter week day the number ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n==6|n==7) Console.WriteLine("It`s weekend now");
+if(n==1|n==2|n==3|n==4|n==5) Console.WriteLine("It`s working day");
