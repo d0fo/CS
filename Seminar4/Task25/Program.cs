ec4c225e3163ac1c41c88b01540d34cccd34e146
d0fo <z_parker@mail.ru>
2022-08-16 11:58:12 +0300

    Task 25 is done.

---
+++ Seminar4/Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number b:");
+int b = Convert.ToInt32(Console.ReadLine());
+int degree = 1;
+for (int i=1; i <= b; i++)
+{
+    degree = degree *a; 
+}
+Console.WriteLine($"Number a in b degeree is: {degree}");
+
