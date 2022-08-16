6d41959fe14516a1a75d497608dc939440475037
d0fo <z_parker@mail.ru>
2022-08-16 16:37:17 +0300

    Task 29 is done.

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -2,18 +2,17 @@
 
 Console.WriteLine("Enter integer number");
 int a = Convert.ToInt32(Console.ReadLine());
-int x=Math.Abs(a);
-string str = x.ToString();
+string str = Math.Abs(a).ToString();
 int[] b = new int[str.Length];
 int sum = 0;
 for( int i=0; i< str.Length; i++)
 {
-     b[i] = int.Parse(str[i].ToString());;
+     b[i] = int.Parse(str[i].ToString());
      sum = sum+ b[i];
 }
 if (a < 0) 
 {
-    sum=sum + (b[0]+b[0])*(-1);
+    sum=sum - b[0]*2;
     Console.WriteLine($"Result is: {sum}");
 }
 else Console.WriteLine($"Sum of the digits of this number is: {sum}");
--- Seminar4/Task29/Program.cs
+++ Seminar4/Task29/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] Array()
+{
+    int[] arr =new int[8];
+    for(int i=0; i<8; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+}
+Console.WriteLine('['+string.Join(", ", Array())+']');
