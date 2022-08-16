7ac8080b3328ad12c2cb12468159cfff6ab80d4c
d0fo <z_parker@mail.ru>
2022-08-16 13:37:58 +0300

    Task 27 is done.

---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter integer number");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a > 0)
+{
+    sum = sum + a % 10;
+    a = a /10 ;
+}
+Console.WriteLine("Sum of the digits in this number is: "+sum);
