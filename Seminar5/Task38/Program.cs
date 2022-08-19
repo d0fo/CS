2a3c2a3b81c3e4a92fa74cdfae65593b610a9818
d0fo <z_parker@mail.ru>
2022-08-19 14:31:34 +0300

    Task 38 remade.

--- Seminar5/Task38/Program.cs
+++ Seminar5/Task38/Program.cs
@@ -1,14 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double[] a = new double [8]; 
+double[] a = new double [8];
+Random rand = new Random();
 double min = 0;
 double max = 0;
     for(int i=0; i<a.Length; i++)
     {
-        a[i] = new Random().Next(0,11);
+        a[i] = rand.Next(0,11) + rand.NextDouble();
         if(a[i]>max) max=a[i];
         if(a[i]<min) min=a[i];
     }
 Console.WriteLine('['+string.Join(", ", a)+']');
-Console.WriteLine($"Difference between max and min elemnt is: {max}-{min}={max-min}");
+Console.WriteLine($"Difference between max and min elemnt is: {max} - {min} = {max-min}");
 
 
