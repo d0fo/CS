de3c50918c5d2f6e5f164dc4b0d811b6cf6ad1bc
d0fo <z_parker@mail.ru>
2022-08-19 09:18:11 +0300

    Task 28 is done.

---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] Array()
+{
+    int[] arr =new int[3];
+    int[] brr = new int[3];
+    for(int i=0; i<arr.Length; i++)
+        {
+            arr[i] = new Random().Next(1,6);
+            int multi=1;
+            for(int j=1; j<=arr[i];j++)
+                {
+                     multi=multi*j;
+                }
+            brr[i]=multi;
+            Console.Write(arr[i]+" ");
+        }
+        return brr;
+        
+}
+Console.WriteLine('['+string.Join(", ", Array())+']');
