a1abdff57823e86abdec063b36453f7c92a0093e
d0fo <z_parker@mail.ru>
2022-08-23 16:03:43 +0300

    Task 38 correcter again.

--- Seminar5/Task38/Program.cs
+++ Seminar5/Task38/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Enter lenght of the massive");
 int n = Convert.ToInt32(Console.ReadLine());
 double[] a = new double [n];
-for(int i =0; i<a.Length;i++)
+for(int i =0; i<=a.Length-1;i++)
     {
         Console.WriteLine($"Enter {i+1} element of massive");
         a[i]=Convert.ToDouble(Console.ReadLine());
@@ -10,7 +10,7 @@ for(int i =0; i<a.Length;i++)
 Console.WriteLine('['+string.Join(", ", a)+']');
 double min = a[0];
 double max = a[0];
-for(int i=0; i<a.Length; i++)
+for(int i=0; i<=a.Length-1; i++)
    {
          if(max<a[i]) max=a[i];
          if(min>a[i]) min=a[i];
