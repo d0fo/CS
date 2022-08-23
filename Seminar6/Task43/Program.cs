4221bdd80c755f7bacc6d3324ac944fe8af106f7
d0fo <z_parker@mail.ru>
2022-08-23 17:11:07 +0300

    Task 43 condition added.

--- Seminar6/Task43/Program.cs
+++ Seminar6/Task43/Program.cs
@@ -7,6 +7,11 @@ Console.WriteLine("Enter k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter b2");
 double b2 = Convert.ToInt32(Console.ReadLine());
-double x =(b2-b1)/(k1-k2);
-double y =k1*(b2-b1)/(k1-k2)+b1;
-Console.WriteLine($"Coordinates of point of intersection of lines is: ({x};{y})");
+if(b1==b2&k1==k2) Console.WriteLine("This functions describe one line");
+if(b1!=b2&k1==k2)Console.WriteLine("Lines are parellel");
+else
+{
+    double x =(b2-b1)/(k1-k2);
+    double y =k1*(b2-b1)/(k1-k2)+b1;
+    Console.WriteLine($"Coordinates of point of intersection of lines is: ({x};{y})");
+}
