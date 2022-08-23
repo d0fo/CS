a3b33bf2910f65756ce512c9aa76f3de9a12af20
d0fo <z_parker@mail.ru>
2022-08-23 16:55:33 +0300

    Task 43 is done.

--- Seminar6/Task43/Program.cs
+++ Seminar6/Task43/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double x =(b2-b1)/(k1-k2);
+double y =k1*(b2-b1)/(k1-k2)+b1;
+Console.WriteLine($"Coordinates of point of intersection of lines is: ({x};{y})");
