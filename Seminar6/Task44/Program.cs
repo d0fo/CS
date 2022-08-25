49c4ceffe930955547b5dead6e1762534998aef9
d0fo <z_parker@mail.ru>
2022-08-25 12:33:16 +0300

    Task44 is done.

--- Seminar6/Task44/Program.cs
+++ Seminar6/Task44/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] fibonacсhi = new int [number];
+fibonacсhi[0]=0;
+fibonacсhi[1]=1;
+for(int i = 2;i<number;i++)
+{
+    fibonacсhi[i]=fibonacсhi[i-1]+fibonacсhi[i-2];
+}
+Console.WriteLine($"First {number} elements of Fibonacсhi sequence are: "+string.Join(", ", fibonacсhi));
