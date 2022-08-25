1725068cf26ac719836edb46c6e061f6d4447a91
d0fo <z_parker@mail.ru>
2022-08-25 14:06:26 +0300

    Additional task is done.

---
+++ Seminar6/AdditionalTask/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Multiplication (int n)
+{
+    if(n==1) return 1;
+    else 
+    {
+        return n*Multiplication(n-1);
+    }
+}
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Product numbers from 1 to your number is: "+Multiplication(Math.Abs(number)));
+
