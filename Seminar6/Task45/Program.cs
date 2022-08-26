7bd8ab39c74c5932ac4d618668887a59a81f528a
d0fo <z_parker@mail.ru>
2022-08-26 15:40:02 +0300

    Task 45 is done

--- Seminar6/Task45/Program.cs
+++ Seminar6/Task45/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int [n];
+int[] b = new int [n];
+for(int i = 0; i <= a.Length-1; i++)
+{
+    a[i] = new Random().Next(-10,11);
+    b[i]=a[i];
+}
+Console.WriteLine('['+string.Join(", ", a)+']');
+Console.WriteLine('['+string.Join(", ", b)+']');
