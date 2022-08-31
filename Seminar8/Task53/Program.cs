c5382665dec29f1f0a2f45835a674e45be74a45c
d0fo <z_parker@mail.ru>
2022-08-31 06:17:01 +0300

    Task 53 54 is done.

---
+++ Seminar8/Task53/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void PrintArray(int[,] matr)
+{
+    for(int i=0; i< matr.GetLength(0);i++)
+    {
+        for (int j=0; j< matr.GetLength(1);j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {   
+            matr[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+void ReplaceRows(int[,]matr)
+{
+    for (int i=0; i<matr.GetLength(1);i++)
+    {
+        int temp = matr[0,i];
+        matr[0,i] = matr[matr.GetLength(0)-1,i];
+        matr[matr.GetLength(0)-1,i]=temp;
+    }
+}
+ReplaceRows(matrix);
+Console.WriteLine();
+PrintArray(matrix); 
---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintArray(int[,] matr)
+{
+    for(int i=0; i< matr.GetLength(0);i++)
+    {
+        for (int j=0; j< matr.GetLength(1);j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {   
+            matr[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+void DescendingOrder(int[,]matr)
+{
+    int temp;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {   
+            for(int k = j+1;k<matr.GetLength(1);k++)
+            {
+                if(matr[i,k]>matr[i,j])
+                {
+                    temp=matr[i,j];
+                    matr[i,j]=matr[i,k];
+                    matr[i,k]=temp;
+                }
+            }
+        }
+    }
+}
+DescendingOrder(matrix);
+Console.WriteLine();
+PrintArray(matrix); 
