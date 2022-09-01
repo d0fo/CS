27a9ab705ec05f72878d10c4e31b32a30799e3a9
d0fo <z_parker@mail.ru>
2022-09-01 14:11:14 +0300

    Task 60 is done.

---
+++ Seminar8/Task60/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintArray(int[,,] matr)
+{
+    for(int i=0; i< matr.GetLength(0);i++)
+    {
+        for (int j=0; j< matr.GetLength(1);j++)
+        {
+            for(int k=0;k<matr.GetLength(2);k++)
+            {
+                Console.Write($"{matr[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,,] matr)
+{
+    for(int i=0; i< matr.GetLength(0);i++)
+    {
+        for (int j=0; j< matr.GetLength(1);j++)
+        {
+            for(int k=0;k<matr.GetLength(2);k++)
+            {
+                matr[i,j,k] = new Random().Next(0,10);
+            }        
+        }
+    }
+}
+int[,,] matrix = new int[2,2,2];
+FillArray(matrix);
+PrintArray(matrix);
