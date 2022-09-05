bc42b779b7b4e07cc69b818281670241bb01ee51
d0fo <z_parker@mail.ru>
2022-09-05 15:11:14 +0300

    Task 64 is done

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+void PrintNum(int n)
+{
+    if(n==0) return;
+    Console.Write(n+" ");
+    PrintNum(n-1);
+}
+PrintNum(number);
