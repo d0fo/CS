d3d4e243b9bf28215aa48b89064ee947079ae9e5
d0fo <z_parker@mail.ru>
2022-09-05 15:32:28 +0300

    Task68 is done

---
+++ Seminar9/Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int AkkermanFunction(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (n > 0 & m == 0)
+    {
+        return AkkermanFunction(n - 1, 1);
+    }
+    else
+    {
+        return AkkermanFunction(n - 1, AkkermanFunction(n, m - 1));
+    }
+
+}
+int m = 2, n = 3;
+Console.WriteLine(AkkermanFunction(n, m));
