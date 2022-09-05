20b58a79227600fd9c1075407c0d3a4f7d684045
d0fo <z_parker@mail.ru>
2022-09-05 15:19:53 +0300

    Task 66 is done.

---
+++ Seminar9/Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter M number:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N number:");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum(int number1, int number2)
+{
+    if (number1 > number2) return 0;
+    return number1 + Sum(number1 + 1, number2);
+}
+
+if (M < N)
+{
+    Console.WriteLine("The sum of elements between M and N is:"+Sum(M, N));
+}
+else
+{
+    Console.WriteLine("The sum of elements between M and N is:"+Sum(N, M));
+}
